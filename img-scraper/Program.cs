835ff251e8b8a118889c24a8dc23b6e14e3ce4f5
Jakub Olech <itsjakeoley@gmail.com>
2020-03-13 21:28:03 +0100

    changed image searching algorithm

--- img-scraper/Program.cs
+++ img-scraper/Program.cs
@@ -7,12 +7,12 @@ using System.Linq;
 using System.Net;
 using System.Security.AccessControl;
 using OfficeOpenXml;
+using System.Text;
 
 namespace img_scraper
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             Console.WriteLine("Wklej adres strony:");
@@ -23,8 +23,9 @@ namespace img_scraper
             };
             List<string> attributes = new List<string>()
             {
-                "data-media-s4", "srcset", "data-src-pc", "src"
+                "data-media-s4", "data-media-s3", "data-media-s2", "data-media-s1", "data-src-mobile", "srcset", "data-src-pc", "src"
             };
+            
             try
             {
                 Directory.CreateDirectory(folder);
@@ -56,7 +57,7 @@ namespace img_scraper
                         var worksheet = excel.Workbook.Worksheets["Images"];
                         List<string[]> rows = new List<string[]>()
                         {
-                            new string[] {"Lp", "Url", "File Name", "Size(kB)", "Alt" }
+                            new string[] {"Lp", "Url", "File name", "File extension", "Size(kB)", "HTML tag", "attribute", "Alt" }
                         };
 
                         HtmlNode html = LoadHtmlDocument(uriResult);
@@ -66,83 +67,76 @@ namespace img_scraper
                             nodes.Add(html);
                         List<string> fileNames = new List<string>();
 
-                        List<HtmlNode> imgNodes = new List<HtmlNode>();
-                        imgNodes.AddRange(nodes.SelectMany(p => p.SelectNodes(".//img") ?? new HtmlNodeCollection(null)));
-                        imgNodes.AddRange(nodes.SelectMany(p => p.SelectNodes(".//picture//source")?? new HtmlNodeCollection(null)));
+                        List<ImageInfo> images = GetImagesInfo(nodes, exts, attributes, subFolder);
                         int i = 1, duplicates = 0;
-                        Console.WriteLine($"Liczba obrazków na stronie: {imgNodes.Count}");
-                        imgNodes.ForEach(t =>
+                        foreach(ImageInfo image in images)
+                        {
+                            string[] row = new string[8];
+
+                            row[0] = i.ToString();
+                            row[1] = image.Uri.ToString();
+                            row[2] = image.FileInfo.Name.Substring(0, image.FileInfo.Name.LastIndexOf("."));
+                            row[3] = image.FileInfo.Extension;
+                            row[5] = image.Name;
+                            row[6] = image.Attribute;
+                            row[7] = image.Alt;
+                            if(fileNames.Contains(image.FileInfo.Name))
                             {
-                                string[] row = new string[5];
-                                foreach (string ext in exts)
-                                {
-                                    string imgUrl="";
-                                    attributes.ForEach(x =>
-                                    {
-                                        if (String.IsNullOrEmpty(imgUrl))
-                                            imgUrl = t.GetAttributeValue(x, "");
-                                    });
-                                    if (!String.IsNullOrEmpty(imgUrl) && imgUrl.Contains(ext))
-                                    {
-                                        imgUrl = imgUrl.Substring(0, imgUrl.LastIndexOf(ext) + ext.Length);
-                                        Uri uriImg;
-                                        if (!(Uri.TryCreate(imgUrl, UriKind.Absolute, out uriImg) && (uriImg.Scheme == Uri.UriSchemeHttp || uriImg.Scheme == Uri.UriSchemeHttps)))
-                                        {
-                                            if (imgUrl.StartsWith("//"))
-                                                imgUrl = "https:" + imgUrl;
-                                            else if (imgUrl.StartsWith("/"))
-                                                imgUrl = "https:/" + imgUrl;
-                                            else
-                                                imgUrl = "https://" + imgUrl;
-                                            try
-                                            {
-                                                uriImg = new Uri(imgUrl);
-                                            }
-                                            catch (Exception e)
-                                            {
-                                                Console.WriteLine(e.Message);
-                                                continue;
-                                            }
-                                        }
-
-                                        row[0] = i.ToString();
-                                        row[1] = uriImg.ToString();
-                                        row[2] = uriImg.ToString().Substring(uriImg.ToString().LastIndexOf("/") + 1);
-                                        row[4] = t.GetAttributeValue("alt", "");
-
-                                        if(fileNames.Contains(row[2]))
-                                        {
-                                            duplicates++;
-                                            continue;
-                                        }
-
-                                        if (!DownloadImage(subFolder, uriImg, new WebClient()))
-                                            continue;
-
-                                        string filePath = subFolder + uriImg.ToString().Substring(uriImg.ToString().LastIndexOf("/"));
-                                        if (File.Exists(filePath))
-                                        {
-                                            FileInfo fi = new FileInfo(filePath);
-                                            row[3] = Math.Round(fi.Length / 1024f).ToString();
-                                            Console.WriteLine($"Saving as {fi.FullName}");
-                                            fileNames.Add(fi.Name);
-                                        }
-                                        rows.Add(row);
-                                        i++;
-                                    }
-                                }
-                            });
+                                duplicates++;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Saving as {image.FileInfo.FullName}");
+                                if (!DownloadImage(image.FileInfo.Directory.ToString(), image.Uri, new WebClient()))
+                                    continue;
+                            }
+                            if (File.Exists(image.FileInfo.FullName))
+                            {
+                                row[4] = Math.Round(image.FileInfo.Length / 1024f).ToString();
+                                fileNames.Add(image.FileInfo.Name);
+                            }
+                            rows.Add(row);
+                            i++;
+                        };
 
                         string headerRange = "A1:" + Char.ConvertFromUtf32(rows[0].Length + 64) + "1";
                         worksheet.Cells[headerRange].LoadFromArrays(rows);
-                        worksheet.Cells[$"A1:{'A'+ i}{i}"].AutoFitColumns();
+                        worksheet.Cells[$"A1:{'A'+ Char.ConvertFromUtf32(rows[0].Length + 64)}{i+1}"].AutoFitColumns();
+
+                        int groupIndex = 0;
+                        foreach (var group in images.GroupBy(x=>x.Parent))
+                        {
+                            if(groupIndex % 2 == 1)
+                            {
+                                int min = 0, max = 0;
+                                min = group.Min(x => images.IndexOf(x))+2;
+                                max = group.Max(x => images.IndexOf(x))+2;
+                                var test = $"A{min}:{'A' + Char.ConvertFromUtf32(rows[0].Length + 64)}{max}";
+                                worksheet.Cells[$"A{min}:{'A' + Char.ConvertFromUtf32(rows[0].Length + 64)}{max}"].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                                worksheet.Cells[$"A{min}:{'A' + Char.ConvertFromUtf32(rows[0].Length + 64)}{max}"].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+                            }
+                            groupIndex += 1;
+                        }
+
+                        string fileName = "";
+                        for (int j = 0; j < uriResult.Segments.Length; j++)
+                        {
+                            fileName += uriResult.Segments[j];
+                        }
+                        fileName = fileName.Replace("/", "-");
+                        if (fileName.Length > 100)
+                            fileName = fileName.Substring(0, 100);
 
                         try
                         {
-                            excel.SaveAs(new FileInfo(subFolder + "images.xlsx"));
-                            Console.WriteLine($"\r\nZnaleziono obrazków: {imgNodes.Count}");
-                            Console.WriteLine($"Duplikatów: {duplicates}");
-                            Console.WriteLine($"Pobrano: {i-1}\r\n\r\nKoniec\r\n\r\nWklej kolejny adres strony");
+                            excel.SaveAs(new FileInfo(Directory.GetCurrentDirectory() + "/Images-cc"+fileName+".xlsx"));
+                            Console.WriteLine($"\r\n\r\nLiczba obrazków na stronie(bez <noscript></noscript>): {images.Count}");
+                            Console.WriteLine($"Liczba tagów <picture>: {images.Where(x => x.Name == "source").GroupBy(x => x.Parent).Select(x => x.First()).Count()}");
+                            Console.WriteLine($"Liczba tagów <img>: {images.Where(x => x.Name == "img" && x.Parent.Name != "picture").GroupBy(x => x.Parent).Select(x => x.First()).Count()}");
+
+                            Console.WriteLine($"\r\nZapisano do .xlsx: { i - 1 }");
+                            Console.WriteLine($"Duplikatów: {duplicates}\r\n");
+                            Console.WriteLine($"Pobrano plików: {fileNames.GroupBy(x => x).Select(x => x.First()).Count()}\r\n\r\nKoniec\r\n\r\nWklej kolejny adres strony");
                         }
                         catch
                         {
@@ -172,6 +166,125 @@ namespace img_scraper
             return documentNode;
         }
 
+        private static List<ImageInfo> GetImagesInfo(List<HtmlNode> htmlNodes, List<string> fileExtensions, List<string> attributesToSearch, string destinationPath)
+        {
+            List<ImageInfo> images = new List<ImageInfo>();
+            foreach(HtmlNode pictureNode in htmlNodes.SelectMany(p => p.SelectNodes(".//picture") ?? new HtmlNodeCollection(null)))
+            { 
+                HtmlNode imgNode = pictureNode.ChildNodes.Where(x => x.Name.Equals("img")).FirstOrDefault();
+                HtmlNode sourceNode = pictureNode.ChildNodes.Where(x => x.Name.Equals("source")).FirstOrDefault();
+                foreach (string attribute in attributesToSearch)
+                {
+                    if (imgNode != null)
+                    {
+                        string imgUrl = imgNode.GetAttributeValue(attribute, "");
+                        if (!String.IsNullOrWhiteSpace(imgUrl))
+                        {
+                            foreach (string extension in fileExtensions)
+                            {
+                                if (!imgUrl.Contains(extension))
+                                    continue;
+                                imgUrl = imgUrl.Substring(0, imgUrl.LastIndexOf(extension) + extension.Length);
+                                Uri uri;
+                                if (!(Uri.TryCreate(imgUrl, UriKind.Absolute, out uri) && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps)))
+                                {
+                                    if (imgUrl.StartsWith("//"))
+                                        imgUrl = "https:" + imgUrl;
+                                    else if (imgUrl.StartsWith("/"))
+                                        imgUrl = "https:/" + imgUrl;
+                                    else
+                                        imgUrl = "https://" + imgUrl;
+                                    try
+                                    {
+                                        uri = new Uri(imgUrl);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine(e.Message);
+                                        continue;
+                                    }
+                                }
+                                images.Add(new ImageInfo(destinationPath + "/" + uri.ToString().Substring(uri.ToString().LastIndexOf("/") + 1), uri, attribute, imgNode.GetAttributeValue("alt", sourceNode.GetAttributeValue("alt", "")), pictureNode, "img"));
+                            }
+                        }
+                    }
+                    if (sourceNode != null)
+                    {
+                        string sourceUrl = sourceNode.GetAttributeValue(attribute, "");
+                        if (!String.IsNullOrWhiteSpace(sourceUrl))
+                        {
+                            foreach (string extension in fileExtensions)
+                            {
+                                if (!sourceUrl.Contains(extension))
+                                    continue;
+                                sourceUrl = sourceUrl.Substring(0, sourceUrl.LastIndexOf(extension) + extension.Length);
+                                Uri uri;
+                                if (!(Uri.TryCreate(sourceUrl, UriKind.Absolute, out uri) && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps)))
+                                {
+                                    if (sourceUrl.StartsWith("//"))
+                                        sourceUrl = "https:" + sourceUrl;
+                                    else if (sourceUrl.StartsWith("/"))
+                                        sourceUrl = "https:/" + sourceUrl;
+                                    else
+                                        sourceUrl = "https://" + sourceUrl;
+                                    try
+                                    {
+                                        uri = new Uri(sourceUrl);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine(e.Message);
+                                        continue;
+                                    }
+                                }
+                                images.Add(new ImageInfo(destinationPath + "/" + uri.ToString().Substring(uri.ToString().LastIndexOf("/") + 1), uri, attribute, sourceNode.GetAttributeValue("alt", imgNode.GetAttributeValue("alt", "")), pictureNode, "source"));
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (HtmlNode imageNode in htmlNodes.SelectMany(p => p.SelectNodes(".//img") ?? new HtmlNodeCollection(null)).Where(x=>!x.ParentNode.Name.Equals("noscript") && !x.ParentNode.Name.Equals("picture")))
+            {
+                foreach (string attribute in attributesToSearch)
+                {
+                    if (imageNode != null)
+                    {
+                        string imgUrl = imageNode.GetAttributeValue(attribute, "");
+                        if (String.IsNullOrWhiteSpace(imgUrl))
+                            continue;
+                        foreach (string extension in fileExtensions)
+                        {
+                            if (!imgUrl.Contains(extension))
+                                continue;
+                            imgUrl = imgUrl.Substring(0, imgUrl.LastIndexOf(extension) + extension.Length);
+                            Uri uri;
+                            if (!(Uri.TryCreate(imgUrl, UriKind.Absolute, out uri) && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps)))
+                            {
+                                if (imgUrl.StartsWith("//"))
+                                    imgUrl = "https:" + imgUrl;
+                                else if (imgUrl.StartsWith("/"))
+                                    imgUrl = "https:/" + imgUrl;
+                                else
+                                    imgUrl = "https://" + imgUrl;
+                                try
+                                {
+                                    uri = new Uri(imgUrl);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine(e.Message);
+                                    continue;
+                                }
+                            }
+                            images.Add(new ImageInfo(destinationPath + "/" + uri.ToString().Substring(uri.ToString().LastIndexOf("/") + 1), uri, attribute, imageNode.GetAttributeValue("alt",  ""), imageNode.ParentNode, "img"));
+                        }
+                    }
+                }
+            }
+            return images;
+        }
+
         private static bool DownloadImage(string folderImagesPath, Uri url, WebClient webClient)
         {
             try
@@ -185,6 +298,35 @@ namespace img_scraper
             }
             return true;
         }
+    }
+
+    public struct ImageInfo
+    {
+        private FileInfo fileInfo;
+        private Uri uri;
+        private string attribute;
+        private string alt;
+        private HtmlNode parent;
+        private string name;
+
+        public ImageInfo(FileInfo fileInfo, Uri uri, string attribute, string alt, HtmlNode parent, string name)
+        {
+            this.parent = parent;
+            this.attribute = attribute;
+            this.alt = alt;
+            this.fileInfo = fileInfo;
+            this.uri = uri;
+            this.name = name;
+        }
+
+        public ImageInfo(string fileName, Uri uri, string attribute, string alt, HtmlNode parent, string name) : this(new FileInfo(fileName), uri, attribute, alt, parent, name) { }
+
+        public FileInfo FileInfo { get => fileInfo; }
+        public Uri Uri { get => uri; }
+        public string Attribute { get => attribute; }
+        public string Alt { get => alt; set => alt = value; }
+        public string Name { get => name;  }
+        public HtmlNode Parent { get => parent; }
     }
 }
 
